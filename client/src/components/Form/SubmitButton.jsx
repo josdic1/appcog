2b17d059def061c698ac7699befc79ebea3de75e
Josh Dicker <joshdicker@mac.mynetworksettings.com>
2025-04-24 09:45:23 -0400

    building POST and offer list

--- client/src/components/Form/SubmitButton.jsx
+++ client/src/components/Form/SubmitButton.jsx
@@ -3,24 +3,19 @@ import BrandContext from "../../contexts/BrandContext"
 import CrudContext from "../../contexts/CrudContext"
 
 function SubmitButton({ formData }) {
-    const { selectedBrand, onBrandSelectEvent } = useContext(BrandContext)
-    const { handleSubmit } = useContext(CrudContext)
+  const { selectedBrand, onBrandSelectEvent } = useContext(BrandContext)
+  const { handleSubmit } = useContext(CrudContext)
 
-    let obj = {}
+  const onClick = () => {
+    const obj = {
+      ...formData,
+      MAKE: selectedBrand
+    }
+    handleSubmit(obj)
+    onBrandSelectEvent("clear", "")
+  }
 
-const onClick = () => {
-        obj = {
-            ...formData,
-            MAKE: selectedBrand
-        }
-      handleSubmit(obj)
-      onBrandSelectEvent("clear", "")
+  return <button type="button" onClick={onClick}>Submit</button>
 }
-
-return (
-<>
-<button type='click' onClick={onClick} >Submit</button>
-</>
-)}
 
 export default SubmitButton
--- client/src/providers/CrudProvider.jsx
+++ client/src/providers/CrudProvider.jsx
@@ -1,12 +1,40 @@
-
+import { useState, useEffect } from "react";
 import CrudContext from "../contexts/CrudContext"
 
 
 function CrudProvider({children}) {
+    const [ offers, setOffers ] = useState([])
+
+    useEffect(() => {
+        fetchOffers()
+    },[])
+
+async function fetchOffers() {
+    try{
+        const r = await fetch(`http://localhost:3000/offers`)
+        if(!r.ok){
+            throw new Error("💥 Error");
+          }
+          const data = await r.json()
+          setOffers(data)
+    }catch (error) {console.error("❌ Caught error:", error);}
+}
 
     async function handleSubmit(obj) {
         try {
-          console.log(obj)
+          const r = await fetch(`http://localhost:3000/offers`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+          })
+          if(!r.ok){
+            throw new Error("💥 Error");
+          }
+          const data = await r.json()
+          const updated = [...offers, data]
+          setOffers(updated)
         }catch (error) {console.error("❌ Caught error:", error);}  
     }
 
